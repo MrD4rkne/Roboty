7652d7ea7a5dffd6528815c79bba8acc00c32fa4
MrD4rkne <marcin.pawel.szopa@gmail.com>
2020-06-15 23:29:02 +0200

    Main x2

--- Roboty/Robot.cs
+++ Roboty/Robot.cs
@@ -8,13 +8,23 @@ namespace Robots
     public class Robot
     {
         private const string Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        /// <summary>
+        /// Zmienna przechowująca nazwę robota
+        /// </summary>
         public string Name { get; private set; }
+        /// <summary>
+        /// Zmienna przechowująca typ robota
+        /// </summary>
         public Type Type { get; private set; }
         public Robot(List<Robot> robots)
         {
             ChangeName(robots);
             this.Type = (Type)new Random().Next(0, 3);
         }
+        /// <summary>
+        /// Funkcja nadająca/zmieniająca nazwę robota
+        /// </summary>
+        /// <param name="robots"></param>
         private void ChangeName(List<Robot> robots)
         {
             string pre_name = string.Empty, new_name = string.Empty;
@@ -32,16 +42,35 @@ namespace Robots
             } while (robots.FindIndex(x => x.Name == new_name) >= 0);
             this.Name = new_name;
             if(pre_name!=string.Empty)
-                Debug.WriteLine("Znieniono nazwę z {0} na {1}",pre_name,this.Name);
+                Debug.WriteLine("Zmieniono nazwę z {0} na {1}",pre_name,this.Name);
         }
+        /// <summary>
+        /// Funkcja do resetowania robota do ustawień fabrycznych
+        /// </summary>
+        /// <param name="robots"></param>
         public void Reset(List<Robot> robots)
         {
             ChangeName(robots);
         }
+        /// <summary>
+        /// Funkcja do konwersji do stringa
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"Name: {Name} Type: {Enum.GetName(typeof(Robots.Type), this.Type)}";
         }
+        public string GetRobotName()
+        {
+            return this.Name;
+        }
+        public Type GetRobotType()
+        {
+            return this.Type;
+        }
     }
+    /// <summary>
+    /// Enum opisujący typy robota
+    /// </summary>
     public enum Type { Translator, CoffeeMaking, Bartender }
 }
