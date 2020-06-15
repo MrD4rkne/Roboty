cb38f4f035e4f8bfbd61ac9af7d9fcdc89db7ded
MrD4rkne <marcin.pawel.szopa@gmail.com>
2020-06-15 23:07:36 +0200

    Dodaj pliki projektów.

---
+++ Roboty/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robots;
+using Roboty;
+namespace Roboty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Robot> robots = new List<Robot>();
+            for (int i = 0; i < 100; i++)
+            {
+                Robot robot = new Robot(robots);
+                robots.Add(robot);
+            }
+            robots[new Random().Next(0, 100)].Reset(robots);
+            foreach(Robot a in robots)
+            {
+                Console.WriteLine(a.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Roboty/Robot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Robots
+{
+    public class Robot
+    {
+        private const string Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public string Name { get; private set; }
+        public Type Type { get; private set; }
+        public Robot(List<Robot> robots)
+        {
+            ChangeName(robots);
+            this.Type = (Type)new Random().Next(0, 3);
+        }
+        private void ChangeName(List<Robot> robots)
+        {
+            string pre_name = string.Empty, new_name = string.Empty;
+            if (this.Name != null)
+                pre_name = this.Name;
+            do
+            {
+                Random rd = new Random();
+                new_name = string.Empty;
+                new_name += Letters[rd.Next(0, Letters.Length)];
+                new_name += Letters[rd.Next(0, Letters.Length)];
+                new_name += (char)rd.Next(48, 58);
+                new_name += (char)rd.Next(48, 58);
+                new_name += (char)rd.Next(48, 58);               
+            } while (robots.FindIndex(x => x.Name == new_name) >= 0);
+            this.Name = new_name;
+            if(pre_name!=string.Empty)
+                Debug.WriteLine("Znieniono nazwę z {0} na {1}",pre_name,this.Name);
+        }
+        public void Reset(List<Robot> robots)
+        {
+            ChangeName(robots);
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name} Type: {Enum.GetName(typeof(Robots.Type), this.Type)}";
+        }
+    }
+    public enum Type { Translator, CoffeeMaking, Bartender }
+}
